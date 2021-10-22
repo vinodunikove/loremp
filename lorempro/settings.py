112cef49a76cde7b39b994fd800cf5febeff291c
unknown <vinodunikove@gmail.com>
2021-10-22 06:05:27 +0530

    first commit

--- lorempro/settings.py
+++ lorempro/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9j%h@1c0_lc2(&)b)ln$nt@_-3d_9o^%etnan(y$9q0(v1gjk3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
